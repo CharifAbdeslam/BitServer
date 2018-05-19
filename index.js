b8be2a038f9ba77ac610c0ba2bd8c354058684f7
Charif Abdeslam <charifabdeslem@gmail.com>
2018-05-19 21:34:18 +0000

    Update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http').Server(app);
 const ws = require('ws');
 const io = require('socket.io')(http);
 const w = new ws('wss://api.bitfinex.com/ws/2');
-
+let chanId={}
 const ethTicker = {
   event: 'subscribe',
   channel: 'ticker',
@@ -14,14 +14,23 @@ const bchTicker = {
   channel: 'ticker',
   symbol: 'tBCHBTC'
 }
-const tickerEthIoEmit = "tickerEth";
-const tickerBchIoEmit = "tickerBch";
-require('./ticker')(ethTicker,http,io,w,tickerEthIoEmit);
-require('./ticker')(bchTicker,http,io,w,tickerBchIoEmit);
-
-
-
-
+  w.on('message', (msg) => {
+      respanse = JSON.parse(msg)
+      if(respanse.pair === 'ETHBTC'){
+        chanId = Object.assign({},chanId,{eth:respanse});
+      }
+      if(respanse.pair === 'BCHBTC'){
+        chanId = Object.assign({},chanId,{bch:respanse});
+      }
+      io.emit("channelID",chanId);
+      if(respanse[1] !== "hb"){
+        io.emit("ticker",respanse);
+      }
+    });
+let eth = JSON.stringify(ethTicker);
+let bch= JSON.stringify(bchTicker);
+w.on('open', () => w.send(eth));
+w.on('open', () => w.send(bch));
 
 http.listen(3001, (err) => {
   console.log("App up and running on port: 3001");
--- ticker.js
+++ ticker.js
@@ -1,13 +1,2 @@
-const ticker = function(ethTicker,http,io,w,tickerEthIoEmit){
-  w.on('message', (msg) => {
-    res = JSON.parse(msg)
-      if(res[1] !=='hb' && res[1]){
-        console.log(res[1])
-          io.emit(tickerEthIoEmit,res[1]);
-       }
-  });
-  let req = JSON.stringify(ethTicker);
-  w.on('open', () => w.send(req));
-  return ticker;
-}
+const ticker =
 module.exports = ticker;
