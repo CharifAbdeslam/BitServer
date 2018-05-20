8e15f6ac43f505c93c8b49d3745ef3b814368712
unknown <charifabdeslem@gmail.com>
2018-05-20 15:07:57 +0100

    Update

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const http = require('http').Server(app);
 const ws = require('ws');
 const io = require('socket.io')(http);
 const w = new ws('wss://api.bitfinex.com/ws/2');
+/*tXRPBTC,tLTCBTC,tXMRBTC,tETCBTC,tIOTBTC,tDSHBTC,tNEOBTC,tZECBTC,tEOSBTC,tOMGBTC*/
 let chanId={}
+
 const ethTicker = {
   event: 'subscribe',
   channel: 'ticker',
@@ -13,7 +15,37 @@ const bchTicker = {
   event: 'subscribe',
   channel: 'ticker',
   symbol: 'tBCHBTC'
-}
+};
+const xrpTicker = {
+  event: 'subscribe',
+  channel: 'ticker',
+  symbol: 'tXRPBTC'
+};
+const ltcTicker = {
+  event: 'subscribe',
+  channel: 'ticker',
+  symbol: 'tLTCBTC'
+};
+const xmrTicker = {
+  event: 'subscribe',
+  channel: 'ticker',
+  symbol: 'tXMRBTC'
+};
+const etcTicker = {
+  event: 'subscribe',
+  channel: 'ticker',
+  symbol: 'tETCBTC'
+};
+const iotTicker = {
+  event: 'subscribe',
+  channel: 'ticker',
+  symbol: 'tIOTBTC'
+};
+const dshTicker = {
+  event: 'subscribe',
+  channel: 'ticker',
+  symbol: 'tDSHBTC'
+};
   w.on('message', (msg) => {
       respanse = JSON.parse(msg)
       if(respanse.pair === 'ETHBTC'){
@@ -22,6 +54,24 @@ const bchTicker = {
       if(respanse.pair === 'BCHBTC'){
         chanId = Object.assign({},chanId,{bch:respanse});
       }
+      if(respanse.pair === 'XRPBTC'){
+        chanId = Object.assign({},chanId,{xrp:respanse});
+      }
+      if(respanse.pair === 'LTCBTC'){
+        chanId = Object.assign({},chanId,{ltc:respanse});
+      }
+      if(respanse.pair === 'XMRBTC'){
+        chanId = Object.assign({},chanId,{xmr:respanse});
+      }
+      if(respanse.pair === 'ETCBTC'){
+        chanId = Object.assign({},chanId,{etc:respanse});
+      }
+      if(respanse.pair === 'IOTBTC'){
+        chanId = Object.assign({},chanId,{iot:respanse});
+      }
+      if(respanse.pair === 'DSHBTC'){
+        chanId = Object.assign({},chanId,{dsh:respanse});
+      }
       io.emit("channelID",chanId);
       if(respanse[1] !== "hb"){
         io.emit("ticker",respanse);
@@ -29,9 +79,20 @@ const bchTicker = {
     });
 let eth = JSON.stringify(ethTicker);
 let bch= JSON.stringify(bchTicker);
+let xrp= JSON.stringify(xrpTicker);
+let ltc= JSON.stringify(ltcTicker);
+let xmr= JSON.stringify(xmrTicker);
+let etc= JSON.stringify(etcTicker);
+let iot= JSON.stringify(iotTicker);
+let dsh= JSON.stringify(dshTicker);
 w.on('open', () => w.send(eth));
 w.on('open', () => w.send(bch));
-
+w.on('open', () => w.send(xrp));
+w.on('open', () => w.send(ltc));
+w.on('open', () => w.send(xmr));
+w.on('open', () => w.send(etc));
+w.on('open', () => w.send(iot));
+w.on('open', () => w.send(dsh));
 http.listen(3001, (err) => {
   console.log("App up and running on port: 3001");
 });
