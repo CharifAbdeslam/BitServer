1817317513ccd5b808690a537055c46e014671de
unknown <charifabdeslem@gmail.com>
2018-05-21 15:17:53 +0100

    Update

--- index.js
+++ index.js
@@ -3,73 +3,31 @@ const http = require('http').Server(app);
 const ws = require('ws');
 const io = require('socket.io')(http);
 const w = new ws('wss://api.bitfinex.com/ws/2');
-/*tXRPBTC,tLTCBTC,tXMRBTC,tETCBTC,tIOTBTC,tDSHBTC,tNEOBTC,tZECBTC,tEOSBTC,tOMGBTC*/
-let chanId={}
 
-const ethTicker = {
-  event: 'subscribe',
-  channel: 'ticker',
-  symbol: 'tETHBTC'
+let chanId={};
+function Payload(channel,symbol){
+  this.event = 'subscribe';
+  this.channel = channel;
+  this.symbol = symbol;
 };
-const bchTicker = {
-  event: 'subscribe',
-  channel: 'ticker',
-  symbol: 'tBCHBTC'
-};
-const xrpTicker = {
-  event: 'subscribe',
-  channel: 'ticker',
-  symbol: 'tXRPBTC'
-};
-const ltcTicker = {
-  event: 'subscribe',
-  channel: 'ticker',
-  symbol: 'tLTCBTC'
-};
-const xmrTicker = {
-  event: 'subscribe',
-  channel: 'ticker',
-  symbol: 'tXMRBTC'
-};
-const etcTicker = {
-  event: 'subscribe',
-  channel: 'ticker',
-  symbol: 'tETCBTC'
-};
-const iotTicker = {
-  event: 'subscribe',
-  channel: 'ticker',
-  symbol: 'tIOTBTC'
-};
-const dshTicker = {
-  event: 'subscribe',
-  channel: 'ticker',
-  symbol: 'tDSHBTC'
-};
-const neoTicker = {
-  event: 'subscribe',
-  channel: 'ticker',
-  symbol: 'tNEOBTC'
-};
-const zecTicker = {
-  event: 'subscribe',
-  channel: 'ticker',
-  symbol: 'tZECBTC'
-};
-const eosTicker = {
-  event: 'subscribe',
-  channel: 'ticker',
-  symbol: 'tEOSBTC'
-};
-const omgTicker = {
-  event: 'subscribe',
-  channel: 'ticker',
-  symbol: 'tOMGBTC'
-};
+const ethTicker = new Payload('ticker','tETHBTC');
+const bchTicker = new Payload('ticker','tBCHBTC');
+const xrpTicker = new Payload('ticker','tXRPBTC');
+const ltcTicker = new Payload('ticker','tLTCBTC');
+const xmrTicker = new Payload('ticker','tXMRBTC');
+const etcTicker = new Payload('ticker','tETCBTC');
+const iotTicker = new Payload('ticker','tIOTBTC');
+const dshTicker = new Payload('ticker','tDSHBTC');
+const neoTicker = new Payload('ticker','tNEOBTC');
+const zecTicker = new Payload('ticker','tZECBTC');
+const eosTicker = new Payload('ticker','tEOSBTC');
+const omgTicker = new Payload('ticker','tOMGBTC');
   w.on('message', (msg) => {
       respanse = JSON.parse(msg)
       if(respanse.pair === 'ETHBTC'){
-        chanId = Object.assign({},chanId,{eth:respanse});
+          if(respanse.channel === 'ticker'){
+            chanId = Object.assign({},chanId,{eth:respanse});
+          }
       }
       if(respanse.pair === 'BCHBTC'){
         chanId = Object.assign({},chanId,{bch:respanse});
@@ -109,31 +67,20 @@ const omgTicker = {
         io.emit("ticker",respanse);
       }
     });
-let eth= JSON.stringify(ethTicker);
-let bch= JSON.stringify(bchTicker);
-let xrp= JSON.stringify(xrpTicker);
-let ltc= JSON.stringify(ltcTicker);
-let xmr= JSON.stringify(xmrTicker);
-let etc= JSON.stringify(etcTicker);
-let iot= JSON.stringify(iotTicker);
-let dsh= JSON.stringify(dshTicker);
-let neo= JSON.stringify(neoTicker);
-let zec= JSON.stringify(zecTicker);
-let eos= JSON.stringify(eosTicker);
-let omg= JSON.stringify(omgTicker);
+    
 w.on('open', () =>{
-  w.send(eth);
-  w.send(bch);
-  w.send(xrp);
-  w.send(ltc);
-  w.send(xmr);
-  w.send(etc);
-  w.send(iot);
-  w.send(dsh);
-  w.send(neo);
-  w.send(zec);
-  w.send(eos);
-  w.send(omg);
+  w.send(JSON.stringify(ethTicker));
+  w.send(JSON.stringify(bchTicker));
+  w.send(JSON.stringify(xrpTicker));
+  w.send(JSON.stringify(ltcTicker));
+  w.send(JSON.stringify(xmrTicker));
+  w.send(JSON.stringify(etcTicker));
+  w.send(JSON.stringify(iotTicker));
+  w.send(JSON.stringify(dshTicker));
+  w.send(JSON.stringify(neoTicker));
+  w.send(JSON.stringify(zecTicker));
+  w.send(JSON.stringify(eosTicker));
+  w.send(JSON.stringify(omgTicker));
 });
 
 http.listen(3001, (err) => {
